60ca26a250c4ebd9a9c2c0ef18a48440f28d9193
YeongjuJeong <81597993+YeongjuJeong@users.noreply.github.com>
2022-01-07 15:38:10 +0900

    Update 23888.cpp

--- PS/BOJ/2022 JAN/23888.cpp
+++ PS/BOJ/2022 JAN/23888.cpp
@@ -7,7 +7,6 @@ ll gcd(ll x, ll y) {
     return gcd(y, x % y);
 }
 
-
 int main() {
     cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(NULL);
@@ -17,13 +16,11 @@ int main() {
 
     while (q--) {
         cin >> x >> l >> r;
-        if (x == 1) {
-            cout << (r - l + 1) * (2 * a + (l - 1) * d + (r - 1) * d) / 2 << '\n';
-        }
+        if (x == 1) cout << (r - l + 1) * (2 * a + (l - 1) * d + (r - 1) * d) / 2 << '\n';
         else { // x == 2
             if (l == r) cout << a + (l - 1) * d << '\n';
             else cout << gcd(a, d) << '\n';
-            // gcd(a, a + d) = gcd(a, d)
+            // gcd(a, a + d) = gcd(a, d) 임을 이용했다
         }
     }
 
